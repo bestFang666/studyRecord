b2d275dcccc5ad35f0f24675cae1694a2c8febca
bestFang666 <3257313360@qq.com>
2022-08-24 21:36:00 +0800

    create the service && mapper

---
+++ src/main/java/com/study/fang/springbootall/service/Imp/UserService.java
@@ -0,0 +1,8 @@
+package com.study.fang.springbootall.service.Imp;
+/**
+ *
+ * @author bestFang666
+ * @date 2022/8/24 21:35
+ */
+public class UserService {
+}
---
+++ src/main/java/com/study/fang/springbootall/service/UserService.java
@@ -0,0 +1,8 @@
+package com.study.fang.springbootall.service;
+/**
+ *
+ * @author bestFang666
+ * @date 2022/8/24 21:35
+ */
+public interface UserService {
+}
